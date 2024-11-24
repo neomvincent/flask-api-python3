0a376c7dc1abe3df1b953ee6dc94790f7abda4ca
vincent@purplepatchservices.com <vincent@purplepatchservices.com>
2024-11-24 17:38:40 +0800

    Adapt Flask app for Vercel serverless function

--- api/index.py
+++ api/index.py
@@ -10,5 +10,8 @@ def home():
 def get_data():
     return jsonify({"data": [1, 2, 3, 4, 5]})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Remove the `app.run()` statement, which isn't needed in serverless deployment
+def handler(req, res):
+    # Flask app serves as a callable function
+    with app.app_context():
+        return app.full_dispatch_request()
